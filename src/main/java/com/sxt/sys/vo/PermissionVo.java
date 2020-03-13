12187712ffd43981d1763156d5169588c7ea1ad3
Graham-myq <1621733261@qq.com>
2020-03-13 17:10:02 +0800

    部门管理删除，以及缓存管理

--- src/main/java/com/sxt/sys/vo/PermissionVo.java
+++ src/main/java/com/sxt/sys/vo/PermissionVo.java
@@ -7,6 +7,6 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class PermissionVo extends Permission {
-
+    
     private static final long serialVersionUID =1L;
 }
